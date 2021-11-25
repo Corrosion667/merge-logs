28c15ddfcdb07b2fc8d209508b4affe0c3c0affb
Artem Nechai <lenikol95@mail.ru>
2021-11-25 23:30:50 +0300

    update error tests

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -11,3 +11,12 @@ def test_merge_logs_wrong_formats():
             'tests/fixtures/unsupported_format.txt',
             'tests/fixtures/1.jsonl',
         )
+
+
+def test_merge_logs_wrong_pass():
+    """Do not allow to pass to script non-existent paths."""
+    with pytest.raises(FileNotFoundError):
+        merge_logs(
+            'tests/fixtures/3.jsonl',
+            'tests/fixtures/1.jsonl',
+        )
